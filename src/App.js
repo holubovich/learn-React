21cfbff8cc92f89eb0760b129545dcec68d3c1ee
Elena Golubovich <elenaholubovich988@gmail.com>
2018-12-06 12:58:30 +0300

    React basics

--- src/App.js
+++ src/App.js
@@ -2,33 +2,68 @@ import React, { Component } from 'react';
 import './App.css';
 import Car from './Car/Car'
 class App extends Component {
+
+  state = {
+    cars: [
+      {name: 'Ford', year: 2018},
+      {name: 'Audi', year: 2016},
+      {name: 'Mazda', year: 2010}
+    ],
+    pageTitle: 'React components',
+    showCars:false
+  }
+  toggleCarsHandler = () => {
+  this.setState({
+    showCars: !this.state.showCars
+  })
+}
+  onChangeName(name,index) {
+
+    const car= this.state.cars[index]
+    car.name=name
+    const cars= [...this.state.cars]
+    cars[index]=car
+    this.setState({cars})
+  }
+
+  deleteHandler(index) {
+    const cars= this.state.cars.concat()
+    cars.splice(index,1)
+   this.setState({cars})
+  }
+
   render() {
-    const divStyle={
-      textAlign:'center'
+    console.log('Render')
+    const divStyle = {
+      textAlign: 'center'
+    }
+let cars=null
+    if(this.state.showCars){
+      cars= this.state.cars.map((car,index) => {
+        return(
+            <Car
+                key={index}
+                name={car.name}
+                year={car.year}
+                onDelete={this.deleteHandler.bind(this,index)}
+                onChangeName={event=> this.onChangeName(event.target.value,index)}
+                />
+        )
+      })
     }
     return (
-      <div style={divStyle}>
-       <h1>Hello world!</h1>
-        <Car name={'Ford'} year={2018}>
-          <p style={{color:'blue'}}>COLOR</p>
-        </Car>
-        <Car name={'Audy'} year={2016}>
-          <p style={{color:'red'}}>COLOR</p>
-        </Car>
-        <Car name={'Mazda'} year={2010}/>
-      </div>
-    );
-    //return React.createElement(
-    //    'div',
-    //    {
-    //      className: "App"
-    //    },
-    //   React.createElement(
-    //       'h1',
-    //       null,
-    //    'Hello world!'
-    //   ))
-  }
-}
+        <div style={divStyle}>
+          <h1>{this.state.pageTitle}</h1>
 
+          <button
+              onClick={this.toggleCarsHandler}
+              >Toggle cars</button>
+
+          {cars}
+
+        </div>
+    );
+  }
+
+}
 export default App;
